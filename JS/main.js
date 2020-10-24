01f2224728013379113553d2f40f58fa31d75575
rybrands301 <59943225+rybrands301@users.noreply.github.com>
2020-10-24 19:57:37 -0400

    Add files via upload

---
+++ JS/main.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>My First Web Page</h1>
+<p>My First Paragraph</p>
+
+
+<script>
+document.getElementById("demo").innerHTML = 5 + 6;
+</script>
+
+</body>
+</html>
